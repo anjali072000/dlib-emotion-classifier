b320083e4153e45f30b3b7e14816d5e355357a20
Biju Borah <biju20_ug@ee.nits.ac.in>
2022-06-26 17:15:32 +0530

    video tester updated

--- video-tester.py
+++ video-tester.py
@@ -18,10 +18,9 @@ face_haar_cascade = cv2.CascadeClassifier(
 labels_class = ['confused', 'engaged',
                 'frustrated', 'Lookingaway', 'bored', 'drowsy']
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
 while True:
-    X = []
     ret, test_img = cap.read()
     if not ret:
         continue
@@ -31,42 +30,42 @@ while True:
     faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)
 
     for (x, y, w, h) in faces_detected:
-        cv2.rectangle(test_img, (x, y), (x+w, y+h), (0, 255, 0), thickness=7)
-
-    resized_img = cv2.resize(test_img, (1000, 700))
-    cv2.imshow('Facial emotion analysis ', resized_img)
+        cv2.rectangle(test_img, (x, y), (x+w, y+h), (0, 255, 0), thickness=3)
 
     if cv2.waitKey(10) == ord('q'):  # wait until 'q' key is pressed
         break
     rects = detector(gray_img, 1)
 
-    if len(rects) == 0:
-        continue
+    for rect in rects:
+        X = []
+        shape = predictor(gray_img, rects[0])
 
-    shape = predictor(gray_img, rects[0])
+        shape_np = np.zeros((68, 2), dtype="int")
 
-    shape_np = np.zeros((68, 2), dtype="int")
+        for i in range(0, 68):
+            shape_np[i] = (shape.part(i).x, shape.part(i).y)
+        shape = shape_np
 
-    for i in range(0, 68):
-        shape_np[i] = (shape.part(i).x, shape.part(i).y)
-    shape = shape_np
+        eucl_dist = euclidean_distances(shape, shape)
+        X.append(eucl_dist)
 
-    eucl_dist = euclidean_distances(shape, shape)
-    X.append(eucl_dist)
+        X = np.array(X)
 
-    X = np.array(X)
+        X_train = tf.expand_dims(X, axis=-1)
 
-    X_train = tf.expand_dims(X, axis=-1)
+        predictions = model.predict(X_train)
 
-    predictions = model.predict(X_train)
+        # Display the landmarks
+        for i, (x, y) in enumerate(shape):
+            # Draw the circle to mark the keypoint
+            cv2.circle(test_img, (x, y), 1, (0, 0, 255), -1)
 
-    # Display the landmarks
-    for i, (x, y) in enumerate(shape):
-        # Draw the circle to mark the keypoint
-        cv2.circle(img, (x, y), 3, (255, 0, 0), -1)
+        cv2.putText(test_img, labels_class[np.argmax(predictions)] + " " + str(round(predictions[0][np.argmax(
+            predictions)] * 100, 2)), (0, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        print("Predicted state: ", labels_class[np.argmax(predictions)])
 
-    print("Predicted state: ", labels_class[np.argmax(predictions)])
-
+    resized_img = cv2.resize(test_img, (1000, 700))
+    cv2.imshow('Facial emotion analysis ', resized_img)
 
 cap.release()
 cv2.destroyAllWindows
